5fd753b15113f5b4cb31d643e4412212a9c521f8
Owen12121212121 <owen12121212121@gmail.com>
2022-04-20 16:38:30 -0700

    Initial Commit

---
+++ PlaneCam.py
@@ -0,0 +1,13 @@
+from winsound import PlaySound
+import jetson.inference
+import jetson.utils
+
+net = jetson.inference.detectNet("coco-airplane", threshold=0.5)
+camera = jetson.utils.videoSource("/dev/video0")      # '/dev/video0' for V4L2
+display = jetson.utils.videoOutput("display://0") # 'my_video.mp4' for file
+
+while display.IsStreaming():
+	img = camera.Capture()
+	detections = net.Detect(img)
+	display.Render(img)
+	display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
